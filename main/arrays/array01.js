ff8a001e077b1c734613d3220ad09a5736d99b6d
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-02 15:45:31 +0530

    another day with js

--- main/arrays/array01.js
+++ main/arrays/array01.js
@@ -29,3 +29,27 @@ const arr = [1,2,3,,5,6,7,7,8,9]
 
 arr.forEach((val) => console.log(val)); // hight order inbuilt function 'forEach'
 
+arr.map((val) => console.log(val))
+
+//  DIFFERENCE BETWEEN MAP AND FOREACH
+
+// Foreach doesn;t return anuy value
+// map return a value , like it returns the value in an array
+
+
+//FUNCTIONS IN AN ARRAY________________________
+
+const newArr = arr.filter(num => num%2==0) // only even part of arr
+
+console.log(newArr)
+
+// filter 
+// slice
+//splice = returns the deleted element in a new array , and performs the dlt operation in the array itself
+
+const num = [1,2,3,4,5,6,7,8,9]
+
+let newarray = num.splice(1,3);
+console.log(newarray)
+console.log(num)
+
