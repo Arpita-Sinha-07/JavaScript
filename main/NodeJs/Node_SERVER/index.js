34e2cf0cfff1dc9d900628a56770752569b369d6
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-06-27 23:20:17 +0530

    Some node basics

--- main/NodeJs/Node_SERVER/index.js
+++ main/NodeJs/Node_SERVER/index.js
@@ -12,18 +12,45 @@
 
 // arpita.listen(8000, () => console.log('started'));
 
-const http = require('http')
+// const http = require('http')
+// const express = require('express')
+
+// const app = express();
+// app.get('/', (req,res) => {
+//     return res.send('Hello from Home page' + ' hi ' + req.query.name); //https://localhost:8000/?name=arpita :=> Hello from Home page hi arpita
+// });
+
+// app.get('/about', (req,res) => {
+//     return res.send('Hello from about page')
+// });
+
+// const myServer = http.createServer(app)
+
+// myServer.listen(8000, () => console.log('Server Started'))
+
+
+//EXPRESS_____
+
+
 const express = require('express')
 
 const app = express();
 app.get('/', (req,res) => {
-    return res.send('Hello from Home page' + ' hi ' + req.query.name); //https://localhost:8000/?name=arpita :=> Hello from Home page hi arpita
+    return res.send('Hello from Home page' + ' hi ' + req.query.name + ' Hello whos age is :' + req.query.age ); //https://localhost:8000/?name=arpita :=> Hello from Home page hi arpita
 });
 
 app.get('/about', (req,res) => {
-    return res.send('Hello from about page')
+    return res.send(`Hello ${req.query.name}`)
 });
 
-const myServer = http.createServer(app)
+app.listen(8000, () => console.log("Server Started"));
 
-myServer.listen(8000, () => console.log('Server Started'))
+// const myServer = http.createServer(app)
+//npm i express@4.18.2 
+// ^5.2.1 => 2.1 can be chnaged but nor 5
+// ^ - install all recommended and minor fixes automatically *5.3.0 ok , 6.2.1 not ok
+//~5.2.1 => .1 can be changed but not 5.2
+// ~ - install all the minor fixes automatically *5.2.4 ok , 5.3.1 not ok = 5.2.x
+// myServer.listen(8000, () => console.log('Server Started'))bash
+
+
