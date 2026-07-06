f8160ae20f76380343d562dade09bff341ee9e4b
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2026-07-06 22:47:18 +0530

    Adding another day with node js

--- main/NodeJs/PROJECT/index.js
+++ main/NodeJs/PROJECT/index.js
@@ -8,7 +8,7 @@ app.get("/api/users", (req, res) => {
     return res.json(users);
 })
 
-app.get('/usershtml', (req,res) => {
+app.get('/users', (req,res) => {
     const html = `
     <ul>
         ${users.map((user) => `<li> ${user.first_name} </li>`).join('')}
@@ -16,6 +16,48 @@ app.get('/usershtml', (req,res) => {
     `;
     res.send(html);
 })
+
+app.get('/users/:id', (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    //TODO : create a new user
+    return res.json({status: 'pending'});
+})
+
+
+app.patch('/users/:id', (req, res) => {
+    //TODO: edit the user with id
+    return res.json({status: 'pending'});
+})
+
+
+app.delete('users/:id', (req, res) => {
+    //TODO: delete the user with id
+    return res.json({status: 'pending'});
+})
+
+// AS THE ROUTE IS SAME SO PERFORMING THE HTTP METHOD WITH THIS ANOTHER WAY
+
+// app
+//     .route('api/users/:id')
+//     .get((req, res) => {
+//         const id = Number(req.params.id);
+//         const user = users.find((user) => user.id === id)
+//         return res.json(user)
+//     })
+//     .patch((req, res) => {
+//         //TODO: edit the user with id
+//         return res.json({status: 'pending'});
+//     })
+//     .delete((req, res) => {
+//         //TODO: delete the user with id
+//         return res.json({status: 'pending'});
+//     })
+
 
 app.listen(PORT, () => console.log(`Server started at PORT : ${PORT}`))
 
